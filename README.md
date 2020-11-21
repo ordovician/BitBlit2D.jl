a51ec25ce62bf4abef84226f4773236d6c2fe6b7
Erik Engheim <erik.engheim@mac.com>
2020-11-21 22:57:15 +0100

    try drawing a line in SDL

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Bit Blit 2D
 
 Experiment with creating a game engine for 2D pixel art style of games. Initially I will just use this as a playground to try out the SDL2 library to draw graphics. 
+
+To test the program, make sure this package is in your path, and enter the Julia REPL:
+
+    julia> using BitBlit2D
+    julia> main()
+    
+This will currently show a window temporarily and draw a line across.
--- src/BitBlit2D.jl
+++ src/BitBlit2D.jl
@@ -1,5 +1,46 @@
 module BitBlit2D
 
-# Write your package code here.
+using SimpleDirectMediaLayer
+const SDL = SimpleDirectMediaLayer
+
+# UInt32(SDL.WINDOW_ALLOW_HIGHDPI|SDL.WINDOW_OPENGL|SDL.WINDOW_RESIZABLE|SDL.WINDOW_SHOWN))
+
+
+function main()
+    ok = SDL.Init(SDL.INIT_VIDEO)
+    win = SDL.CreateWindow(
+            "Hello World", 
+            Int32(100), Int32(100),
+            Int32(300), Int32(400),
+            UInt32(SDL.WINDOW_SHOWN))
+    SDL.SetWindowMinimumSize(win, Int32(50), Int32(50))
+    
+    
+    renderer = SDL.CreateRenderer(win, Int32(-1),
+        UInt32(SDL.RENDERER_ACCELERATED | SDL.RENDERER_PRESENTVSYNC))
+    
+    for i = 1:200
+        x,y = Int[1], Int[1]
+        SDL.PumpEvents()
+        SDL.GetMouseState(pointer(x), pointer(y))
+
+        # Set render color to red ( background will be rendered in this color )
+        SDL.SetRenderDrawColor(renderer, 200, 200, 200, 255)
+        SDL.RenderClear(renderer)
+
+        SDL.SetRenderDrawColor(renderer, 20, 50, 105, 255)
+        SDL.RenderDrawLine(renderer,0,0,800,600)
+
+        rect = SDL.Rect(1,1,200,200)
+        SDL.RenderFillRect(renderer, pointer_from_objref(rect) )
+
+        SDL.RenderPresent(renderer)
+        sleep(0.001)
+    end
+    
+    SDL.Quit()
+end
+
+#main()
 
 end
